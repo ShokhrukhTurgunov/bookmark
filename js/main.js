f178810c0409c054cdb9e578fa0877d6b85727fc
ShokhrukhTurgunov <shohruh.t94@mail.ru>
2021-06-29 15:28:07 +0500

    add site footer html and scss codes

--- js/main.js
+++ js/main.js
@@ -26,3 +26,33 @@ elsTabsLink.forEach(function (link) {
     document.querySelector(link.getAttribute('href')).classList.add('panels__item--active');
   });
 });
+
+
+var elsQuestionTabsItem = document.querySelectorAll('.question-tabs__item');
+var elsQuestionTabsLink = document.querySelectorAll('.question-tabs__link');
+var elsAnswerPanel = document.querySelectorAll('.answer-panels__item');
+
+// Har bir link clickga quloq solsin. Har biriga link deb murojaat qilamiz. O'zimiz nom beramiz
+elsQuestionTabsLink.forEach(function (link) {
+  // Har bir linkka click reaksiyasini beramiz
+  link.addEventListener('click', function (evt) {
+    // Linkning tabiiy reaksiyasi (sakrash)ning oldi olinadi
+    evt.preventDefault();
+    
+    // Hamma li lardan active klassni olib tashlaymiz. Har biriga item deb murojaat qilamiz
+    elsQuestionTabsItem.forEach(function (item) {
+      item.classList.remove('question-tabs__item--active');
+    });
+    
+    // Bosilgan linkning otasiga active klassini qo'shamiz
+    link.parentElement.classList.add('question-tabs__item--active');
+    
+    // Hamma panellarni yopamiz
+    elsAnswerPanel.forEach(function (panel) {
+      panel.classList.remove('answer-panels__item--active');
+    });
+    
+    // Bog'langan panelni topamiz
+    document.querySelector(link.getAttribute('href')).classList.add('answer-panels__item--active');
+  });
+});
